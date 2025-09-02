a25409b5a99631812dfd7f1cbf0b9a178603693a
Ishvendra Singh <ishvendra@gmail.com>
2025-09-02 13:21:13 +0530

    Fix: Updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/sitemap.xml',
+          headers: [
+            {
+              key: 'Content-Type',
+              value: 'application/xml',
+            },
+          ],
+        },
+        {
+          source: '/sitemap-0.xml',
+          headers: [
+            {
+              key: 'Content-Type',
+              value: 'application/xml',
+            },
+          ],
+        },
+      ]
+    },
+  }
+  
+  module.exports = nextConfig
+  
